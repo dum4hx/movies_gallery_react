57e79505029c34c3d71356098c6efad98c577745
SDuqueOrdonez <sduqueordones@gmail.com>
2025-08-26 14:52:19 -0500

    Implement useEffect based counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -6,10 +6,16 @@ import "./App.css";
 // Build card component
 const Card = ({ movie }) => {
   const [liked, setLiked] = useState(false);
+  const [count, setCount] = useState(-1);
+
+  useEffect(() => {
+    setCount((prevCount) => prevCount + 1);
+  }, [liked]);
 
   return (
     <div className="card">
       <h1>{movie.name}</h1>
+      <h1>{count}</h1>
       <h3>{movie.author}</h3>
 
       <button onClick={() => setLiked(!liked)} className="like-btn">

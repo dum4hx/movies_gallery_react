cef704f3f5bbf15772b49f0217437fe6f88e9d90
SDuqueOrdonez <sduqueordones@gmail.com>
2025-08-26 08:55:58 -0500

    Implement useSate

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,33 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 // Build card component
-const Card = () => {
-  return <h3>This is a card</h3>;
+const Card = ({ movie }) => {
+  const [liked, setLiked] = useState(false);
+
+  return (
+    <div className="card">
+      <h1>{movie.name}</h1>
+      <h3>{movie.author}</h3>
+
+      <button onClick={() => setLiked(!liked)} className="like-btn">
+        {liked ? "❤️" : "🤍"}
+      </button>
+    </div>
+  );
 };
 
 // Build app component
 const App = () => {
+  const movies = [
+    { name: "Avatar", author: "Michael Dante DiMartino" },
+    { name: "Avengers Endgame", author: "Crhistopher Markus" },
+    { name: "A beautifull mind", author: "Unkown" },
+  ];
   return (
-    <div>
-      <h1>Card</h1>
-      <Card></Card>
+    <div className="card-container">
+      <Card movie={movies[0]}></Card>
+      <Card movie={movies[1]}></Card>
+      <Card movie={movies[2]}></Card>
     </div>
   );
 };
